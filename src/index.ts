8028ceb96893c983ba4998d0292a01ab4b003f3b
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2025-07-09 19:45:40 +1000

    #OPS-002: setup trpc (#2)
    * install trpc
    
    * add: main app router
    
    * add: default auth router
    
    * ops: tRPC setup
    
    * ops: apply cors on auth apis
    
    * add: main app router to the server

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,27 @@
+import { trpcServer } from '@hono/trpc-server'
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { appRouter } from './server/_index'
 
 const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+
+app.use("/api/auth/**",
+  cors({
+    origin: "localhost:5173",
+    allowHeaders: ["Content-Type", "Authorization"],
+    exposeHeaders: ["Content-Type"],
+    maxAge: 600,
+    credentials: true
+  })
+)
+
+app.use("/trpc/*", cors(),
+  trpcServer({
+    router: appRouter
+  }))
 
 export default app
