ef64b35b400d37d4faa88892d24fe2ef2e8c1992
Peter Pham <pttp15122002@gmail.com>
2025-07-09 19:39:57 +1000

    ops: apply cors on auth apis

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 
 const app = new Hono()
 
@@ -6,4 +7,13 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.use("/api/auth/**",
+  cors({
+    origin: "localhost:5173",
+    allowHeaders: ["Content-Type", "Authorization"],
+    exposeHeaders: ["Content-Type"],
+    maxAge: 600,
+    credentials: true
+  })
+)
 export default app
