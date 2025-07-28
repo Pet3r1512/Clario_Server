8de6fa3504ff545c2c05e5ad4ae13c9c27b87b3e
Peter Pham <pttp15122002@gmail.com>
2025-07-28 13:52:35 +1000

    fix: unable to read trusted origin from env

--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,26 @@ import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 import { appRouter } from './server/_index'
 import 'dotenv/config';
-import { env } from './env';
+import { env } from 'hono/adapter';
 
 const app = new Hono()
 
+
 app.get('/', (c) => {
   return c.text('Hello Hono!')
+
 })
 
-app.use("/api/auth/**",
+app.use("/api/auth/**", async (c, next) => {
+  const trustedOrigin = env<{ TRUSTED_ORIGIN: string }>(c).TRUSTED_ORIGIN || "http://localhost:5173";
   cors({
-    origin: env ? env.TRUSTED_ORIGIN : "http://localhost:5173",
+    origin: trustedOrigin,
     allowHeaders: ["Content-Type", "Authorization"],
     exposeHeaders: ["Content-Type"],
     maxAge: 600,
     credentials: true
-  })
-)
+  })(c, next);
+})
 
 app.use("/trpc/*", cors(),
   trpcServer({
