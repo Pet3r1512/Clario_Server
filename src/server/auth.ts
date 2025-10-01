6d2d428873283d0d89c8278d94eb8f99553e9514
Peter Pham <pttp15122002@gmail.com>
2025-10-01 19:08:30 +1000

    add: callback url to client

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -26,7 +26,7 @@ export const authRouter = router({
         const { email, password } = input
         const response = await auth.api.signInEmail({
             body: {
-                email, password
+                email, password, callbackURL: process.env.TRUSTED_ORIGIN || "http://localhost:5173/dashboard"
             }
         })
         if (!response) {
