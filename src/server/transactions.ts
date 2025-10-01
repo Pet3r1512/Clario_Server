18f9c7bbdb2818567c328759604b65865a803ac8
Peter Pham <pttp15122002@gmail.com>
2025-10-01 19:35:18 +1000

    hotfix: wrong env variables

--- src/server/transactions.ts
+++ src/server/transactions.ts
@@ -4,14 +4,35 @@ import { publicProcedure, router } from "./tRPC";
 export const transactionsRouter = router({
     createDefaultCategories: publicProcedure.mutation(async ({ }) => {
         await prisma.$connect()
-        const response = await prisma.categories.create({
+        const USER_ID = "Wm1zO1KHImNFRWpPxbJ3kQ2UoU7pA0Bg"
+
+        const salaryCategory = await prisma.category.findFirst({
+            where: { name: "Salary", userId: null },
+        });
+
+        const foodCategory = await prisma.category.findFirst({
+            where: { name: "Food & Drinks", userId: null },
+        });
+
+        await prisma.transaction.create({
             data: {
-                id: 1,
-                name: "Demo 1",
-                type: "INCOME",
-                description: "demo desc"
+                userId: USER_ID,
+                amount: 1750.00,
+                description: "Salary for first half of October",
+                date: new Date("2025-09-29"),
+                categoryId: salaryCategory?.id,
+
             }
         })
-        console.log(response)
+
+        await prisma.transaction.create({
+            data: {
+                userId: USER_ID,
+                amount: 10.5,
+                description: "Top Tea",
+                date: new Date("2025-09-30"),
+                categoryId: foodCategory?.id ?? undefined,
+            }
+        })
     })
 })
