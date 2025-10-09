daca77f86bdb2cb68f7dc3ec41d6b55bf6ac7cbf
Peter Pham <pttp15122002@gmail.com>
2025-10-09 20:07:04 +1100

    ops: add context to tRPC routes

---
+++ src/server/context.ts
@@ -0,0 +1,7 @@
+import { CreateExpressContextOptions } from "@trpc/server/adapters/express";
+
+export function createContext({ req, res }: CreateExpressContextOptions) {
+    return { req, res };
+}
+
+export type Context = ReturnType<typeof createContext>;
