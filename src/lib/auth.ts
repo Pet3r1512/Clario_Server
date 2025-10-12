2004200e4e356b24f554fa0287a2984a5981e15e
Peter Pham <pttp15122002@gmail.com>
2025-10-12 20:04:21 +1100

    chore: remove unused and add config for cookie

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -15,28 +15,39 @@ export const auth = betterAuth({
     session: {
         expiresIn: 60 * 60 * 24 * 7 //.    7 days
     },
-    advanced: {
-        useSecureCookies: process.env.NODE_ENV === "production",
-        crossSubDomainCookies: {
-            enabled: true,
+    cookieOptions: {
+        secure: false,
+        sameSite: "lax",
+        path: "/",
+    },
+    // advanced: {
+    //     useSecureCookies: process.env.NODE_ENV === "production",
+    //     crossSubDomainCookies: {
+    //         enabled: true,
+    //     },
+    //     defaultCookieAttributes: {
+    //         httpOnly: true,
+    //         secure: process.env.NODE_ENV === "production",
+    //         sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    //         path: "/"
+    //     },
+    //     cookies: {
+    //         session_token: {
+    //             name: "Clario",
+    //             attributes: {
+    //                 httpOnly: true,
+    //                 secure: process.env.NODE_ENV === "production",
+    //                 sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    //                 path: "/"
+    //             }
+    //         }
+    //     }
+    // },
+    adapter: {
+        fetch: async (request: string | Request | URL) => {
+            const response = await fetch(request);
+            return new Response(await response.text(), response);
         },
-        defaultCookieAttributes: {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-            path: "/"
-        },
-        cookies: {
-            session_token: {
-                name: "Clario",
-                attributes: {
-                    httpOnly: true,
-                    secure: process.env.NODE_ENV === "production",
-                    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-                    path: "/"
-                }
-            }
-        }
     },
 })
 
