bba96af9ae1f020f4161934e3931ebfa8fe0a2fa
Peter Pham <pttp15122002@gmail.com>
2025-10-15 18:44:40 +1100

    imp: remove auth from tRPC

--- src/server/_index.ts
+++ src/server/_index.ts
@@ -1,9 +1,7 @@
-import authRouter from './auth';
 import { transactionsRouter } from './transactions';
 import { router } from './tRPC';
 
 export const appRouter = router({
-    auth: authRouter,
     transactions: transactionsRouter
 });
 
