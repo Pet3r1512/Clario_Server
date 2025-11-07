d535a3a58ca6ddef8300eca7e5e33060fe762055
Peter Pham <pttp15122002@gmail.com>
2025-11-07 19:57:06 +1100

    ops: adjust order of middleware

--- src/index.ts
+++ src/index.ts
@@ -14,18 +14,30 @@ const app = new Hono<{
 app.use(
   "*",
   cors({
-    origin: ["http://localhost:5173", "https://clario-web.pages.dev"],
-    allowHeaders: ["Content-Type", "Authorization"],
-    allowMethods: ["POST", "GET", "OPTIONS"],
+    origin: [
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+      "https://clario-web.pages.dev",
+    ],
+    allowHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Requested-With",
+      "Accept",
+      "Origin",
+      "User-Agent",
+      "Referer",
+    ],
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     exposeHeaders: ["Content-Length", "Set-Cookie"],
     credentials: true,
     maxAge: 600,
   })
 );
 
-app.get('/', (c) => {
-  return c.text("Hello")
-})
+app.get("/", (c) => {
+  return c.text("Hono server is running");
+});
 
 app.use(
   "/trpc/*",
@@ -39,12 +51,15 @@ app.all("/api/auth/*", (c) => auth.handler(c.req.raw));
 
 app.use("*", async (c, next) => {
   const session = await auth.api.getSession({ headers: c.req.raw.headers });
-
-  c.set("user", session?.user ?? null);
-  c.set("session", session?.session ?? null);
-
   console.log(session)
-
+  if (!session) {
+    c.set("user", null);
+    c.set("session", null);
+    await next();
+    return;
+  }
+  c.set("user", session.user);
+  c.set("session", session.session);
   await next();
 });
 
@@ -52,7 +67,9 @@ app.get("/session", (c) => {
   const session = c.get("session");
   const user = c.get("user");
 
-  if (!user) return c.body(null, 401);
+  if (!user) {
+    return c.json({ message: "Not authenticated" }, 401);
+  }
 
   return c.json({ session, user });
 });
