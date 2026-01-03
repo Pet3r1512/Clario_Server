b7bca3ae0f828f45550f38afd8b2ccb232041643
Peter Pham <pttp15122002@gmail.com>
2026-01-03 23:04:41 +1100

    ops: update prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,18 +1,10 @@
-import { PrismaClient } from '@prisma/client';
-import { PrismaNeon } from '@prisma/adapter-neon';
-import { neonConfig } from '@neondatabase/serverless';
+import "dotenv/config";
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '../generated/prisma/client'
 
-neonConfig.poolQueryViaFetch = true;
+const connectionString = `${process.env.DATABASE_URL}`
 
-const connectionString = process.env.DATABASE_URL;
+const adapter = new PrismaPg({ connectionString })
+const prisma = new PrismaClient({ adapter })
 
-if (!connectionString) {
-    throw new Error('DATABASE_URL is not defined');
-}
-
-const poolConfig = { connectionString };
-const adapter = new PrismaNeon(poolConfig);
-
-const prisma = new PrismaClient({ adapter });
-
-export default prisma;
+export { prisma }
