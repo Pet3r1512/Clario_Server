fc72cba34e2707a801753857237209981ee3d56a
Peter Pham <pttp15122002@gmail.com>
2026-01-04 10:46:17 +1100

    fix: prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,5 @@
+import { PrismaClient } from '@/generated/prisma'
 import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from "@/generated/prisma/client";
-// import { PrismaClient } from "@/generated/prisma";
 
 const connectionString = `${process.env.DATABASE_URL}`
 
