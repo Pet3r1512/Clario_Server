1d393b604bb5d6e8c41930e10d17513ceac85ece
Peter Pham <pttp15122002@gmail.com>
2026-01-03 23:04:24 +1100

    chore: leave empty procedure for now

--- src/server/transactions.ts
+++ src/server/transactions.ts
@@ -1,42 +1,6 @@
-import prisma from "@/lib/prisma";
+import { prisma } from "@/lib/prisma";
 import { publicProcedure, router } from "./tRPC";
 
 export const transactionsRouter = router({
-    createDefaultCategories: publicProcedure.mutation(async ({ }) => {
-        const USER_ID = "Wm1zO1KHImNFRWpPxbJ3kQ2UoU7pA0Bg"
 
-        const salaryCategory = await prisma.category.findFirst({
-            where: { name: "Salary", userId: null },
-        });
-
-        const foodCategory = await prisma.category.findFirst({
-            where: { name: "Food & Drinks", userId: null },
-        });
-
-        await prisma.transaction.create({
-            data: {
-                userId: USER_ID,
-                amount: 1750.00,
-                description: "Salary for first half of October",
-                date: new Date("2025-09-29"),
-                categoryId: salaryCategory?.id,
-
-            }
-        })
-
-        await prisma.transaction.create({
-            data: {
-                userId: USER_ID,
-                amount: 10.5,
-                description: "Top Tea",
-                date: new Date("2025-09-30"),
-                categoryId: foodCategory?.id ?? undefined,
-            }
-        })
-    }),
-    getTransactions: publicProcedure.query(async ({ }) => {
-        const transactions = await prisma.transaction.findMany()
-
-        return { transactions: transactions }
-    })
 })
