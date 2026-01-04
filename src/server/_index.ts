c81867b8454d11364c659fb2170ff10f66c34ee1
Peter Pham <pttp15122002@gmail.com>
2026-01-04 13:56:20 +1100

    ops: add balance's router to tRPC

--- src/server/_index.ts
+++ src/server/_index.ts
@@ -1,8 +1,10 @@
+import { balancesRouter } from './balance';
 import { transactionsRouter } from './transactions';
 import { router } from './tRPC';
 
 export const appRouter = router({
-    transactions: transactionsRouter
+    transactions: transactionsRouter,
+    balances: balancesRouter
 });
 
 export type AppRouter = typeof appRouter
