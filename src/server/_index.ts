98606e5efa5ecc7c8408b45567d3954c7aa35588
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2026-01-05 20:58:12 +1100

    #OPS-012: seeding default categories (#14)
    * ops: procedure for adding default global categories
    
    * add: categories router to trpc app router

--- src/server/_index.ts
+++ src/server/_index.ts
@@ -1,10 +1,12 @@
 import { balancesRouter } from './balance';
+import { categoriesRouter } from './categories';
 import { transactionsRouter } from './transactions';
 import { router } from './tRPC';
 
 export const appRouter = router({
     transactions: transactionsRouter,
-    balances: balancesRouter
+    balances: balancesRouter,
+    categories: categoriesRouter
 });
 
 export type AppRouter = typeof appRouter
