7c18cbe063c86aed594475e311ab86058128c8f5
Peter Pham <pttp15122002@gmail.com>
2026-01-15 09:28:31 +1100

    add: get session middleware

--- src/index.ts
+++ src/index.ts
@@ -33,6 +33,20 @@ app.use(
   }),
 );
 
+app.use("*", async (c, next) => {
+  const session = await auth.api.getSession({ headers: c.req.raw.headers });
+  if (!session) {
+    c.set("user", null);
+    c.set("session", null);
+    await next();
+    return;
+  }
+  c.set("user", session.user);
+  c.set("session", session.session);
+  await next();
+});
+
+
 app.get("/", (c) => {
   return c.json({
     message: "Hono server is running",
