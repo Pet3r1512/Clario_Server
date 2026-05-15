8765deed6c4f95ca49dbd06a6fae5d0819bbce49
Peter Pham <pttp15122002@gmail.com>
2026-05-15 20:15:25 +1000

    chore: remove unsupport singleton on workers

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,26 +1,19 @@
-import { PrismaClient } from "@prisma/client"
-import { PrismaNeon } from "@prisma/adapter-neon"
-import { neonConfig } from "@neondatabase/serverless"
+import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { neonConfig } from "@neondatabase/serverless";
 
-neonConfig.poolQueryViaFetch = true
+neonConfig.poolQueryViaFetch = true;
 
-const connectionString = process.env.DATABASE_URL
+const connectionString = process.env.DATABASE_URL;
 if (!connectionString) {
-    throw new Error("DATABASE_URL is not defined")
+    throw new Error("DATABASE_URL is not defined");
 }
 
-const adapter = new PrismaNeon({ connectionString })
+const adapter = new PrismaNeon({ connectionString });
 
-let client: PrismaClient | null = null
+const prisma = new PrismaClient({
+    adapter,
+    log: ["error"],
+});
 
-export function getPrismaClient() {
-    if (!client) {
-        client = new PrismaClient({
-            adapter,
-            log: ["error"],
-        })
-    }
-    return client
-}
-
-export default getPrismaClient()
+export default prisma;
